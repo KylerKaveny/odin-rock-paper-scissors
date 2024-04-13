c2a635d60e12e77f025d5f78ac77f3cbc999871c
KylerKaveny <Kkaveny@gmail.com>
2024-04-13 11:39:42 -0700

    Add a ui to the rock-paper-scissors program
    Add a styles.css file to style the div. Change the
    script.js file to update the game display based on
    DOM methods. Link the styles.css file to the
    index.html.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+let computerScore = 0;
+let playerScore = 0;
+let roundResult = '';
+
 //Initalize getComputerChoice function
 
 function getComputerChoice() {
@@ -14,15 +19,19 @@ function roundOfGame (playerSelection, computerSelection) {
         return "This round is a tie"
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        playerScore++;
         return "You win! " + playerSelection + " beats " + computerSelection;
     }
     else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        playerScore++;
         return "You win! " + playerSelection + " beats " + computerSelection;
     }
     else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        playerScore++;
         return "You win! " + playerSelection + " beats " + computerSelection;
     }
     else {
+        computerScore++;
         return "You lose! " + computerSelection + " beats " + playerSelection;
     }
 
@@ -33,6 +42,36 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+const rockButton = document.querySelector("button.rock-button");
+rockButton.addEventListener("click", () => onClick("Rock"));
+
+const paperButton = document.querySelector("button.paper-button");
+paperButton.addEventListener("click", () => onClick("Paper"));
+
+const scissorsButton = document.querySelector("button.scissors-button");
+scissorsButton.addEventListener("click",  () => onClick("Scissors"));
+
+
+function updateResultDisplay () {
+    const displayResult = document.querySelector("div.displayResult");
+    if (computerScore < 5 && playerScore < 5) {
+        displayResult.innerHTML = roundResult + "<br/>" + "The current score is Player: " + playerScore +
+        " Computer: " + computerScore;
+    }
+    else if (computerScore > playerScore) {
+        displayResult.innerHTML = roundResult + "<br/>" + "The computer was the first to reach five wins! Sorry, you lose!";
+    }
+
+    else {
+        displayResult.innerHTML = roundResult + "<br/>" + "You were the first to reach five wins! Congrats you win!";
+    }
+}
+
+function onClick (playerChoice) {
+    roundResult = (roundOfGame(playerChoice, getComputerChoice()));
+    updateResultDisplay ();
+}
+/*
 //Play a game of rock-paper-scissors
 //Initalize the function
 function playGame() {
@@ -49,7 +88,8 @@ function playGame() {
         let gameRoundResult = roundOfGame(playerSelection, computerChoice);
         console.log(gameRoundResult);
 
-        //If player wins add 1 count to player's win count else add 1 count to computer's win count
+        //If player wins add 1 count to player's win count else add 1 count to 
+        // computer's win count
         if (gameRoundResult.charAt(4) === "w") {
             playerWinCount++;
         }
@@ -67,3 +107,4 @@ function playGame() {
         return "It was a tie out of five.";
     }
 }
+*/
